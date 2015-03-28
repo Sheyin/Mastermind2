d9876a7938b44134a3201ed3cd6782e0fda0540a
Sheyin <Sheyin@users.noreply.github.com>
2015-03-28 16:01:52 -0500

    Update mastermind.hpp

--- mastermind.hpp
+++ mastermind.hpp
@@ -5,26 +5,21 @@
 
 class Mastermind {
 	private:
+	    int turn;
 		std::string answer[4];
-		int turn;
 		std::string guessedCode[4];
 		std::string results[4];
 
 	public:
 	    Mastermind();       // default constructor
 		std::string validColors[6];
-		//std::string validColors[6] = {"red", "orange", "yellow", "green", "blue", "purple"};
 		std::string intToColor(int number);
 		void guessPegs();
 		bool inProgress();
 		bool isInvalidColor(std::string color);
 		void printArray(std::string printMe[]);
+		void showRules();
 		void resultsArray(std::string guessArray[]);
-
-		//not sure what I was thinking with these:
-		//std::string dropSymbols(std::string guess);
-		//std::string dropSymbols(std::string guess, int length);
-		//std::string lowerCase(std::string color);
-		//std::string lowerCase(std::string color, int toCheck);
-
+		std::string makeLower(std::string original);
+        //std::string validColors[6] = {"red", "orange", "yellow", "green", "blue", "purple"};
 };
